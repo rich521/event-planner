e24e2df111643504b5037b2eb049e31afbc47192
Richard Strutt <richardastrutt@gmail.com>
2016-06-03 21:52:43 +0800

    moved task runner files to root directory

--- gulpfile.js
+++ gulpfile.js
@@ -13,12 +13,12 @@ var gulp = require('gulp'),
 gulp.task('default', ['styles', 'lint', 'compress', 'minify-css', 'minify-html'], function () {
 
     browserSync.init({
-        server: "./"
+        server: "./src"
     });
 
-    gulp.watch('js/**/*.js', ['lint', 'compress']).on('change', browserSync.reload);
-    gulp.watch('sass/**/*.scss', ['styles', 'minify-css']);
-    gulp.watch("*.html", ['minify-html']).on('change', browserSync.reload);
+    gulp.watch('src/js/**/*.js', ['lint', 'compress']).on('change', browserSync.reload);
+    gulp.watch('src/sass/**/*.scss', ['styles', 'minify-css']);
+    gulp.watch("src/*.html", ['minify-html']).on('change', browserSync.reload);
 });
 
 gulp.task('lint', function () {
@@ -26,7 +26,7 @@ gulp.task('lint', function () {
     // So, it's best to have gulp ignore the directory as well. 
     // Also, Be sure to return the stream from the task; 
     // Otherwise, the task may end before the stream has finished. 
-    return gulp.src(['js/**/*.js', '!js/ie10-bug.js', '!node_modules/**'])
+    return gulp.src(['src/js/**/*.js', 'src/!js/ie10-bug.js', '!node_modules/**'])
         // eslint() attaches the lint output to the "eslint" property 
         // of the file object so it can be used by other modules. 
         .pipe(eslint())
@@ -39,32 +39,32 @@ gulp.task('lint', function () {
 });
 
 gulp.task('styles', function () {
-    gulp.src('sass/**/*.scss')
+    gulp.src('src/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
             browsers: ['last 2 versions']
         }))
-        .pipe(gulp.dest('./css'))
+        .pipe(gulp.dest('src/css'))
         .pipe(browserSync.stream());
 });
 
 gulp.task('compress', function () {
-    return gulp.src('js/app.js')
+    return gulp.src('src/js/app.js')
         .pipe(babel({
             presets: ['es2015']
         }))
         .pipe(uglify().on('error', gutil.log))
-        .pipe(gulp.dest('../dist/js'));
+        .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('minify-css', function () {
-    return gulp.src('css/*.css')
+    return gulp.src('src/css/*.css')
         .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(gulp.dest('../dist/css'));
+        .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('minify-html', function () {
-    return gulp.src('*.html')
+    return gulp.src('src/*.html')
         .pipe(htmlmin({ collapseWhitespace: true }))
-        .pipe(gulp.dest('../dist'))
+        .pipe(gulp.dest('./dist'))
 });
